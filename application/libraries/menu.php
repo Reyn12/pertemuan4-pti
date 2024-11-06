2e22e9a5cf592af89bb5804e4d4cba4b00be42e6
Reyn12 <muhammadrenaldi687@gmail.com>
2024-11-06 15:14:57 +0700

    fix bug export excell

--- application/libraries/menu.php
+++ application/libraries/menu.php
@@ -1,22 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Menu{
-	var $CI = NULL;
-	function __construct()
-	{
-		// get CI's object
-		$this->CI =& get_instance();
-	}
-	function tampil_sidebar()
-	{
-		// load model 'usermodel'
-		$this->CI->load->model('usermodel');
-		// level untuk user ini
-		$level = $this->CI->session->userdata('level');
-		// ambil menu dari database sesuai dengan level
-		$data['menu'] = $this->CI->usermodel->get_menu_for_level($level);
-		$data['level'] = $this->CI->session->userdata('level');
 
-		// tampilkan halaman dashboard dengan data menu
-		$this->CI->load->view('sidebar-nav', $data);
-	}
+class Menu
+{
+    var $CI = NULL;
+
+    function __construct()
+    {
+        // Get CI's object
+        $this->CI =& get_instance();
+    }
+
+    // Display the sidebar menu
+    function tampil_sidebar()
+    {
+        // Load 'usermodel' model
+        $this->CI->load->model('usermodel');
+        
+        // Get user level from session
+        $level = $this->CI->session->userdata('level');
+        
+        // Get the menu for the user based on their level
+        $data['menu'] = $this->CI->usermodel->get_menu_for_level($level);
+        $data['level'] = $this->CI->session->userdata('level');
+
+        // Display the sidebar using the data
+        $this->CI->load->view('sidebar-nav', $data);
+    }
 }
--- application/libraries/template.php
+++ application/libraries/template.php
@@ -1,29 +1,33 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 /**
- * CODEIGNITER template library
-13
+ * CODEIGNITER Template Library
  *
  * @author Jérôme Jaglale
  * @url http://maestric.com/doc/php/codeigniter_template
  */
 class Template
 {
-	var $template_data = array();
+    var $template_data = array();
 
-	function set($name, $value)
-	{
-		$this->template_data[$name] = $value;
-	}
+    // Set template data
+    function set($name, $value)
+    {
+        $this->template_data[$name] = $value;
+    }
 
-	function load($template = '', $view = '' , $view_data = array(),
-				  $return = FALSE)
-	{
-		$this->CI =& get_instance();
-		$this->set('contents', $this->CI->load->view($view, $view_data,
-			TRUE));
-		return $this->CI->load->view($template, $this->template_data,
-			$return);
-	}
+    // Load the template and view
+    function load($template = '', $view = '', $view_data = array(), $return = FALSE)
+    {
+        $this->CI =& get_instance();
+        
+        // Set the contents of the view
+        $this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
+        
+        // Load the template with the view data
+        return $this->CI->load->view($template, $this->template_data, $return);
+    }
 }
+
 /* End of file Template.php */
 /* Location: ./application/libraries/Template.php */
